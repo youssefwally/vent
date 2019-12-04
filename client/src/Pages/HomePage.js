ebc52ca0660edacc69ab4fb6745df2000798bf0c
MoAtefAmer <mo.atefamer@gmail.com>
2019-12-04 17:00:52 +0100

    Backend Methods edits

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
